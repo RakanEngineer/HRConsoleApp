b8c5049f9f499ca4d790820be593c5df17d40d0b
RakanEngineer <rakan.engineer@gmail.com>
2025-11-04 14:47:58 +0100

    Add salary

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@
             Console.WriteLine("Enter empolyee name");
             string name = Console.ReadLine();
             Console.WriteLine($"Welcome to HR System, {name}!");
+
+            Console.WriteLine("Enter empolyee salary");
+            decimal salary = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine($"Your sallary is {salary:C}");
         }
     }
 }
