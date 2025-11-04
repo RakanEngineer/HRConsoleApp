67ac39817d0883016aa4a6fa816cfc6e1de5bed9
RakanEngineer <rakan.engineer@gmail.com>
2025-11-04 14:45:13 +0100

    Add empolyee name

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter empolyee name");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Welcome to HR System, {name}!");
         }
     }
 }
