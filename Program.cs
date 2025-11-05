5fcbb4d5212469c650e13287204361562e1f8a91
RakanEngineer <rakan.engineer@gmail.com>
2025-11-05 15:53:01 +0100

    Comment Roboty things from Program.cs

--- Program.cs
+++ Program.cs
@@ -17,16 +17,16 @@ namespace HRConsoleApp
             robot.Age = -5;
             Console.WriteLine($"Robot Name: {robot.GetName()}\n");
 
-            Robot r1 = new Robot("R1", 25, 25000);
-            Console.WriteLine(Robot.Count);
-            Robot r2 = new Robot("R2", 25, 25000);
-            Console.WriteLine(Robot.Count);
-            Robot r3 = new Robot("R3", 25, 25000);
-            Console.WriteLine(Robot.Count);
-            Robot r4 = new Robot("R4", 25, 25000);
-            Console.WriteLine(Robot.Count);
-            Robot r5 = new Robot("R5", 25, 25000);
-            Console.WriteLine(Robot.Count);
+            //Robot r1 = new Robot("R1", 25, 25000);
+            //Console.WriteLine(Robot.Count);
+            //Robot r2 = new Robot("R2", 25, 25000);
+            //Console.WriteLine(Robot.Count);
+            //Robot r3 = new Robot("R3", 25, 25000);
+            //Console.WriteLine(Robot.Count);
+            //Robot r4 = new Robot("R4", 25, 25000);
+            //Console.WriteLine(Robot.Count);
+            //Robot r5 = new Robot("R5", 25, 25000);
+            //Console.WriteLine(Robot.Count);
 
             Dictionary<string, decimal> employees = new Dictionary<string, decimal>();
             bool running = true;
